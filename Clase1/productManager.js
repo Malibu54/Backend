39c698392eed37087190b9e758161891617834c7
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-11-16 21:26:46 -0300

    rename folder

--- Clase1/productManager.js
+++ Clase1/productManager.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 class ProductManager {
     constructor() {
         this.products = [];
---
+++ Clase1/prueba.js
@@ -0,0 +1,16 @@
+class ProductManager {
+    constructor(product){
+        this.product = []
+
+    }
+
+}
+
+const product = newProduct {
+    "title" =  this.product,
+    "description" =
+    "price" =
+    "thumbnail" =
+    "code" =
+    "stock" =
+}
