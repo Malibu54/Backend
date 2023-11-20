66fa3523e01c25d546bf34959aa12c2e320232cc
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-11-20 11:50:59 -0300

    add view code

---
+++ Proyecto/src/routes/views.router.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const router = express.Router();
+let clothes = [
+  { name: "Zapatillas", price: "11" },
+  { name: "Pantalones", price: "22" },
+  { name: "Medias", price: "33" },
+  { name: "Lana", price: "33" },
+  { name: "Almohadas", price: "55" },
+];
+
+router.get("/", (req, res) => {
+  let testUser = {
+    name: "Hilda",
+    last_name: "Martinez",
+    role: "Admin",
+  };
+  res.render("índex", {
+    user: testUser,
+    isAdmin: testUser.role === "Admin",
+    clothes,
+  });
+});
+
+export default router;
