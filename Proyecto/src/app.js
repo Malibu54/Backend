9bcfc21cc78e4fccf9b9c32dc893cfbe39780b0d
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-11-20 11:35:15 -0300

    add url

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import ProductManager from "./ProductManager";
 import handlebars from "handlebars";
-import {__dirname} from "./utils"
+import __dirname from "./utils"
 
 
 const app = express();
